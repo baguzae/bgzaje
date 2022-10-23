c7842627a38e563be30c5e4faa84a7eeb2b147ac
baguzae <110842227+baguzae@users.noreply.github.com>
2022-10-24 00:06:45 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -15,7 +15,7 @@ let str = `${global.wm}
 │ • *Ovo:* [${global.povo}]
 │ • *Link Aja:* [${global.plinkaja}]
 ❏────`
-let wibu = `https://i.ibb.co/P5BBzbf/286637238-392674839545088-948545114162206238-n.jpg` 
+let wibu = `https://i.ibb.co/tsTmKg9/20220817-225030.jpg` 
 let thumb = await(await fetch(wibu)).buffer()
 conn.sendButtonDoc(m.chat, str, wm,'Sewa Bot','.sewa', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
     mediaUrl: "https://Instagram.com/baguzae11_",
