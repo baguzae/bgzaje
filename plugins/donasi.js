5949e60a485378943f119307b3893b2cef40efec
baguzae <110842227+baguzae@users.noreply.github.com>
2022-10-23 23:40:27 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -6,7 +6,7 @@ let handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
 let str = `${global.wm}
 
 ┌─「 Donasi • Pulsa 」
-│ • *Telkomsel:* [${global.ppulsa}]
+│ • *Indosat:* [${global.ppulsa}]
 ❏────
 
 ┌─「 Donasi • Non Pulsa 」
@@ -18,10 +18,10 @@ let str = `${global.wm}
 let wibu = `https://i.ibb.co/P5BBzbf/286637238-392674839545088-948545114162206238-n.jpg` 
 let thumb = await(await fetch(wibu)).buffer()
 conn.sendButtonDoc(m.chat, str, wm,'Sewa Bot','.sewa', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
-    mediaUrl: "https://Instagram.com/_ctzhid",
+    mediaUrl: "https://Instagram.com/baguzae11_",
     mediaType: "VIDEO",
-    description: "https://www.instagram.com/p/CevoCg5hG-p/?utm_source=ig_web_copy_link", 
-    title: 'WH-MODS-DEV-V1 MultiDevice',
+    description: "https://www.instagram.com/p/CgCGyFdP5hA/?igshid=YmMyMTA2M2Y=", 
+    title: '𝐁𝐆𝐙 𝐁𝐎𝐓𝐙 𝐕𝟓 - 𝕭𝖆𝖌𝖚𝖟𝕬𝕰',
     body: wm,
     thumbnail: thumb,
     sourceUrl: sig
